00996fec8f812cd6ac7d3d48f49d9c1f237211bd
trepachkoDm <56437385+trepachkoDm@users.noreply.github.com>
2020-03-16 22:47:29 +0300

    Add files via upload

---
+++ RainbowFrame/components/MainBlock.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+import RainbowFrame from './RainbowFrameComponent';
+
+class MainBlock extends React.Component {
+    render () {
+        let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+        return (
+            <RainbowFrame colors={colors}>
+                Hello!
+            </RainbowFrame>
+        );
+    }
+}
+
+export default MainBlock;
---
+++ RainbowFrame/components/RainbowFrameComponent.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Rainbow.css';
+
+class RainbowFrame extends React.Component {
+    static propTypes = {
+        colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    };
+    render () {
+        let code =this.props.children;
+        this.props.colors.forEach( color => {
+            code = <div style={{border:"solid 3px "+color,padding:"5px"}}>{code}</div>
+        } );
+        return (
+            code
+        )
+    }
+}
+
+export default RainbowFrame;
