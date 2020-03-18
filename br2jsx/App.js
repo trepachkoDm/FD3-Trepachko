7e04dada07611e32ea1cc6fa647326ee3bf49077
trepachkoDm <56437385+trepachkoDm@users.noreply.github.com>
2020-03-18 09:34:45 +0300

    Add files via upload

---
+++ br2jsx/App.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainBlock from './components/MainBlock';
+
+
+ReactDOM.render(
+    <MainBlock/>,
+    document.getElementById('container')
+);
+
---
+++ br2jsx/components/MainBlock.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+import BR2JSX from './Br2jsx';
+
+class MainBlock extends React.Component {
+    render () {
+        let text="первый<br>второй<br/>третий<br />последний";
+        return <BR2JSX text={text}/>;
+    }
+}
+
+export default MainBlock;
